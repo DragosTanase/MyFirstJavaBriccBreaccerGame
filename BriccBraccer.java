27cdc45bcb036ec366c09d3f0e58de2d101d633b
atmosphericsoul <gandalfeluldemere@gmail.com>
2022-04-29 23:39:35 +0300

    Update BriccBraccer.java

--- BriccBraccer.java
+++ BriccBraccer.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package briccbraccer;
 
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
-/**
- *
- * @author ganda
- */
 public class BriccBraccer {
 
    
